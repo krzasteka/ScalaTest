254fffafb570a5a61df811297dfca06394a7ac8a
Andrzej Krzastek <krzasteka@gmail.com>
2016-06-22 00:07:13 -0700

    wokring cache

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -12,22 +12,27 @@ import play.api.mvc._
 
 import play.api.libs.ws._
 import play.api.http.HttpEntity
+import play.api.cache._
 
 import akka.actor.{ActorSystem, ActorRef, Props}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
 import akka.util.ByteString
 import akka.routing.RoundRobinPool
+
 import concurrent.Future
+import concurrent.duration._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
-class HomeController @Inject()(ws: WSClient, system: ActorSystem) extends Controller {
+class HomeController @Inject()(ws: WSClient, system: ActorSystem, configuration: play.api.Configuration, cache: CacheApi) extends Controller {
   
+  val numOfActors = configuration.underlying.getString("routerConfig.numOfActors").toInt 
+  val cacheDuration = configuration.underlying.getString("cache.duration").toInt
   implicit val actorSystem = ActorSystem("coding-challenge-system")
-  val esActorsRouter = actorSystem.actorOf(RoundRobinPool(5).props(Props[ElasticsearchActor]), "esRouter")
-  val kActorsRouter = actorSystem.actorOf(RoundRobinPool(5).props(Props[KafkaActor]), "kRouter")
+  val esActorsRouter = actorSystem.actorOf(RoundRobinPool(numOfActors).props(Props[ElasticsearchActor]), "esRouter")
+  val kActorsRouter = actorSystem.actorOf(RoundRobinPool(numOfActors).props(Props[KafkaActor]), "kRouter")
       
   def getRequest(path: String) = Action.async {
     request => { 
@@ -36,49 +41,42 @@ class HomeController @Inject()(ws: WSClient, system: ActorSystem) extends Contro
         
         request.method match {
           case "GET" =>{
-            ws.url(reqUrl).get().map { response =>
-              val wsResponse = WebRequest(reqUrl, response.body, response.status)
-              val wrl  = WebRequestLog(requestor, wsResponse)
-              esActorsRouter ! wrl
-              kActorsRouter ! wrl
-              Ok("Status: " + response.status.toString)
-            }  
+            cachedOrStore(request, reqUrl, requestor)
           }
           
           case "PUT" =>{
-            ws.url(reqUrl).execute("PUT").map { response =>
-              val wsResponse = WebRequest(reqUrl, response.body, response.status)
-              val wrl  = WebRequestLog(requestor, wsResponse)
-              esActorsRouter ! wrl
-              kActorsRouter ! wrl
-              Ok("Status: " + response.status.toString)
-            }  
+            cachedOrStore(request, reqUrl, requestor) 
           }
           
           case "POST" =>{
-            ws.url(reqUrl).execute("POST").map { response =>
-              val wsResponse = WebRequest(reqUrl, response.body, response.status)
-              val wrl  = WebRequestLog(requestor, wsResponse)
-              esActorsRouter ! wrl
-              kActorsRouter ! wrl
-              Ok("Status: " + response.status.toString)
-            }  
+            cachedOrStore(request, reqUrl, requestor) 
           }
           
           case "DELETE" =>{
-            ws.url(reqUrl).execute("DELETE").map { response =>
-              val wsResponse = WebRequest(reqUrl, response.body, response.status)
-              val wrl  = WebRequestLog(requestor, wsResponse)
-              esActorsRouter ! wrl
-              kActorsRouter ! wrl
-              Ok("Status: " + response.status.toString)
-            }  
+            cachedOrStore(request, reqUrl, requestor) 
           }
           
           case _ => Future {
-                      Ok("oops invalid request method: " + request.method) 
-                    }
+            Ok("oops invalid request method: " + request.method) 
+          }
       }
+    }
+  }
+  
+  def cachedOrStore(request: Request[AnyContent], reqUrl: String, requestor: WebRequest): Future[Result] = {
+    val cached = cache.get[Any](request.method+reqUrl)
+    cached match {
+      case Some(cachedThing) => Future { Ok(cachedThing.toString) } 
+      case None => {
+        ws.url(reqUrl).execute(request.method).map { response =>
+          val wsResponse = WebRequest(reqUrl, response.body, response.status)
+          val wrl  = WebRequestLog(requestor, wsResponse)
+          esActorsRouter ! wrl
+          kActorsRouter ! wrl
+          cache.set(request.method+reqUrl,response.body, cacheDuration.minutes)
+          Ok("Status: " + response.status.toString)
+        }
+      } 
     }
   }
 
